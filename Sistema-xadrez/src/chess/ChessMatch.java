31dce12507bd68329952adff4ca98a9389a011c4
Joserem <jrsliva@outlook.com>
2024-04-16 10:39:04 -0300

    Colocando as peças no tabuleiro

--- Sistema-xadrez/src/chess/ChessMatch.java
+++ Sistema-xadrez/src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.chess_pieces.King;
+import chess.chess_pieces.Rook;
 
 public class ChessMatch {
 
@@ -12,6 +15,7 @@ public class ChessMatch {
     
     public ChessMatch(){
         board = new Board(8, 8);
+        initialSetup();
     }
 
     // Methods
@@ -30,6 +34,11 @@ public class ChessMatch {
         }
         return mat;
 
+    }
+
+    private void initialSetup(){
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+        board.placePiece(new King(board, Color.WHITE), new Position(0, 4));
     }
 
 
